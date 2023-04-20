a02263d7088f1d6361747246910a79ceaea1c3a4
BhavyaP45 <127996743+BhavyaP45@users.noreply.github.com>
2023-04-20 07:36:26 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem'
+     },
+
+     colors: {
+      'base-red': '#E71D36',
+      'default-background': '#011627',
+      'end-gradient': '#37182B',
+      'teal-accent': '#00FFE7',
+      'yellow': '#FFBF1C',
+      'white': '#FFFFFF',
+    },
+
+    fontFamily: {
+      'Montserrat': ''
+    },
+
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+ }
+
+
